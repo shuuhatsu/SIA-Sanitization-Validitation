2bc6a5e2af20cda900a9b4e5e331aaa2ab8390de
Joshua Amper <159984024+shuuhatsu@users.noreply.github.com>
2024-05-31 22:03:44 +0800

    SIA-San-Valid

---
+++ SIA---Validation-Sanitization-main/SIA---Validation-Sanitization-main/auth_app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Rules\StrongPassword;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * Display the registration view.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('Auth/Register');
+    }
+
+    /**
+     * Handle an incoming registration request.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        //Validation
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'password' => ['required', 'confirmed', 'min:8', new StrongPassword],
+        ]);
+
+        //Sanitization
+        $request->merge([
+            'name' => trim($request->name),
+            'email' => filter_var($request->email, FILTER_SANITIZE_EMAIL),
+        ]);
+
+        //Create User
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        event(new Registered($user));
+
+        Auth::login($user);
+
+        return redirect(route('dashboard', absolute: false));
+    }
+}
---
+++ SIA---Validation-Sanitization-main/SIA---Validation-Sanitization-main/auth_app/app/Rules/StrongPassword.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class StrongPassword implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        return preg_match('/[!@#$%^&*(),.?":{}|<>]/', $value);
+    }
+
+    public function message()
+    {
+        return 'The :attribute must contain at least one special character.';
+    }
+}
+
